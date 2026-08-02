e0f2204d2245091a17c06c2caeae6bc75471fae8
licol <licol@example.com>
2026-08-02 09:36:15 +0200

    finished the js for all the section, starting css

---
+++ blog.js/private/books.js
@@ -0,0 +1,56 @@
+const listArticles = [
+
+]
+
+//setting the pagination
+
+const articlesPerPage = 8;
+let currentPage = 1;
+
+// we bring the html div in here
+
+const containerArticles = document.getElementById('articles-container');
+const containerPagination = document.getElementById('pagination-container');
+
+//function for the articles visibility
+
+function articleProjection() {
+    containerArticles.innerHTML = "";
+    const indexBegin = (currentPage - 1) * articlesPerPage;
+    const indexEnd = indexBegin + articlesPerPage;
+    const articlesOfThePage = listArticles.slice(indexBegin, indexEnd);
+    articlesOfThePage.forEach(articles => {
+        containerArticles.innerHTML += `
+        <small>${articles.date}</small>
+        <h2>${articles.title}</h2>
+        <p>${articles.teaser}</p>
+        <a href="${articles.lien}">Read the entire article</a>
+        `;
+    });
+    genererPagination();
+}
+
+function genererPagination() {
+    containerPagination.innerHTML = "";
+    const totalPages = Math.ceil(listArticles.length / articlesPerPage);
+
+    for (let i = 1; i <= totalPages; i++) {
+        const button = document.createElement('button');
+        button.textContent = i;
+        button.style.margin = "0 5px";
+
+        if (i === currentPage) {
+            button.classList.add('active');
+        }
+    
+
+    button.addEventListener('click', () => {
+        currentPage = i;
+        articleProjection();
+    });
+
+    containerPagination.appendChild(button);
+    }
+}
+
+articleProjection();
